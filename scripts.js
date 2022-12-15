5b1b7f45038b919b4bfbbc5300460db9e6ea2e13
Volodymyr Hromiak <vgromyak87@gmail.com>
2022-12-15 09:05:50 +0200

    fix algorythm

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 let getArguments1 = +prompt("Input first number",'');
 let getArguments2 = +prompt("Input second number",'');
 
-alert( getArguments1 + getArguments2 );
+
+function calcSum(a, b) {
+	return a+b;
+}
+
+alert( calcSum(getArguments1, getArguments2) );
